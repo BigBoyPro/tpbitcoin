e67fbf11f1ea138152426fd975d95cb7d429c6f3
BAGHDADI Wassim <wassim.baghdadi@etu.univ-amu.fr>
2024-03-26 01:31:13 +0100

    KKK

--- app/src/main/java/tpbitcoin/App.java
+++ app/src/main/java/tpbitcoin/App.java
@@ -24,81 +24,67 @@ import java.util.List;
 
 public class App {
 
-
-
     public static void main(String[] args) {
 
-        //Q1  hashrate
-        System.out.println("\nQ1:");
-        double localHashrate = new HashRateEstimator(500,5).estimate();
+        // Q1 hashrate
+        System.out.println("\nQ1");
+        double estimatedHashrate = new HashRateEstimator(500,5).estimate();
         System.out.print("Hashrate: ");
-        System.out.println(localHashrate);
+        System.out.println(estimatedHashrate);
 
-        // Q2: latest  block  from mainet (bitcoin blockchain) and its predecessor
-        Context context   = new Context(new UnitTestParams()); // required  for working with bitcoinj
+        // Q2: latest block from mainet (bitcoin blockchain) and its predecessor
+        Context context = new Context(new UnitTestParams()); // required for working with bitcoinj
         Explorer explorer = new Explorer(); // for interacting with blockchain.info API
-        String latestBlockHash = explorer.getLatestHash();
-        Block latestBlock = explorer.getBlockFromHash(context.getParams(), latestBlockHash);
+        String latestHash = explorer.getLatestHash();
+        Block latestBlock = explorer.getBlockFromHash(context.getParams(), latestHash);
 
-        System.out.println("\nQ2:");
+        System.out.println("\nQ2");
         System.out.print("Nonce: ");
         System.out.println(latestBlock.getNonce());
         System.out.print("Niveau de difficulté: ");
         System.out.println(latestBlock.getDifficultyTargetAsInteger());
 
         // Q3 Some TXs
-        System.out.println("\nQ3:");
+        System.out.println("\nQ3");
         System.out.print("Transaction 1: ");
         System.out.println(latestBlock.getTransactions().get(0).getHashAsString());
         System.out.print("Transaction 2: ");
         System.out.println(latestBlock.getTransactions().get(1).getHashAsString());
 
-
         // Q4 Mine a new block
         Miner miner = new Miner(context.getParams());
-        // empty list of tx since creating txs, even fake ones, requires some work
         ArrayList<Transaction> txs = new ArrayList<>();
-        // TODO : mine a new block
+        // TODO: mine a new block
 
-
-        //Q5
+        // Q5
         System.out.println("\nQ5");
-        System.out.println("Non, mon bloc ne serait pas accepté par le " +
-                "réseau car il ne répond pas à la difficulté actuelle du réseau Bitcoin " +
-                "et manque la preuve de travail valide requise.");
+        System.out.println("Non, mon bloc ne serait pas accepté par le réseau car il ne répond pas à la difficulté actuelle du réseau Bitcoin et manque la preuve de travail valide requise.");
 
-
-
-        //Q6
+        // Q6
         System.out.println("\nQ6");
-        //en utilisant le site https://www.stelareum.io/mining/gpu.html j'ai obtenu les hashrates de ma carte graphique
-        long hashrate = 45_000_000; // 45 MH/s
-        System.out.println("Avec un hashrate de 45 MH/s et le niveau de difficulté donné, " +
-                "il me faudrait des années pour miner un bloc seul");
+        long gpuHashrate = 45_000_000; // 45 MH/s
+        System.out.println("Avec un hashrate de 45 MH/s et le niveau de difficulté donné, il me faudrait des années pour miner un bloc seul");
 
-
-        //Q7
+        // Q7
         System.out.println("\nQ7");
-
         BigInteger difficulty = BigInteger.valueOf(2).pow(20);
-        long expectedTimeSeconds = ImpactUtils.expectedMiningTime(hashrate, difficulty);
-        double years = expectedTimeSeconds / (365.25 * 24 * 3600);
-        System.out.println("Temps moyen pour miner un bloc sur ma machine : " + years + " années");
-
+        long predictedTimeSeconds = ImpactUtils.expectedMiningTime(gpuHashrate, difficulty);
+        double estimatedYears = predictedTimeSeconds / (365.25 * 24 * 3600);
+        System.out.print("Temps moyen pour miner un bloc sur ma machine : ");
+        System.out.println(estimatedYears + " years");
 
         // Q8
         System.out.println("\nQ8");
-        BigInteger currentDifficulty = latestBlock.getDifficultyTargetAsInteger();
-        double networkHashrate = ImpactUtils.networkHashrate(currentDifficulty);
-        System.out.println("Hashrate du réseau : " + networkHashrate + " h/s");
+        BigInteger blockDifficulty = latestBlock.getDifficultyTargetAsInteger();
+        double totalNetworkHashrate = ImpactUtils.calculateNetworkHashrate(blockDifficulty);
+        System.out.print("Hashrate du réseau : ");
+        System.out.println(totalNetworkHashrate + " h/s");
 
         // Q9/Q10 energy w/ most profitable hardware
         System.out.println("\nQ9/Q10");
-        double c = 100;
-        double p = 2000;
-        double energyConsumed = ImpactUtils.energyConsumedLast24h(c, p);
-        System.out.println("Énergie consommée par le réseau bitcoin dans les dernières 24h: " + energyConsumed + " kWh");
-
+        double networkEnergyUsage = ImpactUtils.calculateEnergyConsumptionLast24h(100, 2000);
+        System.out.print("Énergie consommée par le réseau bitcoin dans les dernières 24h: ");
+        System.out.println(networkEnergyUsage + " kWh");
 
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(YearMonth.class,new YearMonthAdapter())
@@ -110,13 +96,7 @@ public class App {
             Type listType = new TypeToken<ArrayList<MiningHardware>>(){}.getType();
             hardwares = gson.fromJson(reader,listType);
         } catch (Exception e) {
-            System.err.println("error opening/reading hardware.json "+ e.getMessage());
+            System.err.println("error opening/reading hardware.json " + e.getMessage());
         }
-
-
     }
-
-
-
-
 }
--- app/src/main/java/tpbitcoin/ImpactUtils.java
+++ app/src/main/java/tpbitcoin/ImpactUtils.java
@@ -4,19 +4,17 @@ import java.math.BigInteger;
 
 public class ImpactUtils {
 
-
     private static final long AVERAGE_BLOCK_TIME_SECONDS = 600; // 10 minutes in seconds
+
     /**
-     * computes the expected time (in seconds) for mining a block
+     * Computes the expected time (in seconds) for mining a block
      * @param hashrate: miner hashing capacity (hash/s)
      * @param difficultyAsInteger: block difficulty, as BigInteger (256 bits integer)
      * @return expected time in seconds before finding a correct block
      */
-    // TODO
     public static long expectedMiningTime(long hashrate, BigInteger difficultyAsInteger){
-        double nombreMoyenhash = 1.0 / calculerProbabiliteMinage(difficultyAsInteger);
-        return (long) (nombreMoyenhash / hashrate);
-
+        double averageHashCount = 1.0 / calculateMiningProbability(difficultyAsInteger);
+        return (long) (averageHashCount / hashrate);
     }
 
     /**
@@ -24,45 +22,36 @@ public class ImpactUtils {
      * @param difficultyAsInteger: difficulty level as 256bits integer
      * @return hashrate of the network in GH/s
      */
-    // TODO
-    public static long  globalHashRate(BigInteger difficultyAsInteger){
+    public static long globalHashRate(BigInteger difficultyAsInteger){
         return (long) (Math.pow(2, 22) / difficultyAsInteger.doubleValue());
     }
 
     /**
-     * Compute the total energy consumption of the network
-     * assuming each miner has the same hashrate, and consume the same power
+     * Compute the total energy consumption of the network assuming each miner has the same hashrate, and consumes the same power
      * @param minerHashrate: the hashrate of each miner, in GH/s
      * @param minerPower: the power consumption of each miner, in Watts
      * @param networkHashrate : the global hashrate of the network
-     * @param duration : in second
+     * @param duration : in seconds
      * @return energy consumed during duration, in kWh
      */
-    // TODO
     public static long globalEnergyConsumption(long minerHashrate, long minerPower, long networkHashrate, long duration){
         double totalPower = (double) (networkHashrate / minerHashrate) * minerPower;
         return (long) (totalPower * (duration / 3600.0));
     }
 
-    private static double calculerProbabiliteMinage(BigInteger difficultyAsInteger) {
-        BigInteger nombreMaxHashs = BigInteger.valueOf(2).pow(256);
-        return difficultyAsInteger.doubleValue() / nombreMaxHashs.doubleValue();
+    private static double calculateMiningProbability(BigInteger difficultyAsInteger) {
+        BigInteger maxHashes = BigInteger.valueOf(2).pow(256);
+        return difficultyAsInteger.doubleValue() / maxHashes.doubleValue();
     }
 
-
-    public static double networkHashrate(BigInteger difficultyAsInteger) {
+    public static double calculateNetworkHashrate(BigInteger difficultyAsInteger) {
         BigInteger hashrate = BigInteger.valueOf(2).pow(256).divide(difficultyAsInteger.multiply(BigInteger.valueOf(AVERAGE_BLOCK_TIME_SECONDS)));
         return hashrate.doubleValue();
     }
 
-    public static double energyConsumedLast24h(double c, double p) {
+    public static double calculateEnergyConsumptionLast24h(double c, double p) {
         double powerInKW = p / 1000.0;
         double hashPerDay = c * 1_000_000_000 * 24 * 60 * 60;
         return hashPerDay * powerInKW / 1_000;
     }
-
-
-
-
-
 }
